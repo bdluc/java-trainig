3cbac3d8f318d54d625a23c57e868ab0d4630b95
taitruong2320 <52031560+taitruong2320@users.noreply.github.com>
2021-11-23 16:25:13 +0700

    restapi

---
+++ java/RestApiConnectDB/src/test/java/com/tma/RestApiConnectDB/RestApiConnectDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tma.RestApiConnectDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiConnectDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
