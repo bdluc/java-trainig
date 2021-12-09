d6dbac20d853cc144908fe864bd50d1d8211b205
nguyenductinh1998py <nguyenductinhtaiphuyen71@gmail.com>
2021-12-09 20:07:45 +0700

    log4j xml

--- SpringExampleHaveLog4J/src/com/spring/log4j/MainApp.java
+++ SpringExampleHaveLog4J/src/com/spring/log4j/MainApp.java
@@ -2,15 +2,13 @@ package com.spring.log4j;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 public class MainApp {
-	static Logger log = LogManager.getLogger(MainApp.class.getName());
+	static Logger log = Logger.getLogger(MainApp.class.getName());
 	private static ApplicationContext context;
 	
 	public static void main(String[] args) {
-		
 		context = new ClassPathXmlApplicationContext("Beans.xml");
 		log.debug("This is debug");
 		log.info("Will create Book book");
